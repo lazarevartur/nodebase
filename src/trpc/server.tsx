173206e7fdb2b23f2da333f81ecd17a19fe3a21c
lazarevartur <gracian950@gmail.com>
2025-12-24 17:02:51 +0200

    feat: implement TRPC integration for user data fetching and set up client component

--- src/trpc/server.tsx
+++ src/trpc/server.tsx
@@ -4,7 +4,7 @@ import { cache } from 'react';
 import { createTRPCContext } from './init';
 import { makeQueryClient } from './query-client';
 import { appRouter } from './routers/_app';
-import { createTRPCClient, httpLink } from '@trpc/client';
+
 // IMPORTANT: Create a stable getter for the query client that
 //            will return the same client during the same request.
 export const getQueryClient = cache(makeQueryClient);
@@ -13,10 +13,5 @@ export const trpc = createTRPCOptionsProxy({
   router: appRouter,
   queryClient: getQueryClient,
 });
-// If your router is on a separate server, pass a client:
-createTRPCOptionsProxy({
-  client: createTRPCClient({
-    links: [httpLink({ url: '...' })],
-  }),
-  queryClient: getQueryClient,
-});
+
+export const caller = appRouter.createCaller(createTRPCContext);
